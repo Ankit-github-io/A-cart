fb97379337e78c14acb5796da4f125379649589d
Ankit Maurya <119157881+Ankit-github-io@users.noreply.github.com>
2023-10-10 01:06:59 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,7 +5,7 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite:'Lax',
+    sameSite:'none',
     secure:true
   };
   res.status(statusCode).cookie("token", token, options).json({
