5c5950b1108fa286c8ec4a4e8371c48a5d38b49b
Ankit <Ankitmaurya734801@gmail.com>
2023-10-09 18:32:28 +0530

    fix webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,79 @@
+// const path = require("path");
+// const htmlPlugin = require("html-webpack-plugin");
+// module.exports = {
+//   resolve: {
+//     extensions: [".*", ".js", ".jsx", ".css", ".scss"],
+//     alias: {
+//       component: path.resolve(__dirname, "../src/components"),
+//       assets: path.resolve(__dirname, "../src/assets"),
+//       reducers: path.resolve(__dirname, "../src/reducers"),
+//       //routes: path.resolve(__dirname, '../src/routes'),
+//       //lib: path.resolve(__dirname, '../src/lib'),
+//       //mainstyles: path.resolve(__dirname, './../src/styles/mainStyles.css'),
+//     },
+//   },
+
+//   mode: "production",
+//   entry: "./src/index.js",
+//   output: {
+//     path: path.resolve(__dirname, "dist"),
+//     filename: "bundle.js",
+//     //publicPath:'/'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//         },
+//       },
+
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+
+//         use: ["style-loader", "css-loader"],
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|ico|gif)$/,
+//         use: {
+//           loader: "file-loader",
+//           options: {
+//             name: "[name].[hash].[ext]",
+//             outputPath: "assets/images",
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new htmlPlugin({
+//       template: "./public/index.html",
+//     }),
+//   ],
+//   performance: {
+//     hints: false,
+//     maxEntrypointSize: 512000,
+//     maxAssetSize: 512000,
+//   },
+
+//   //line of sequence matters
+//   devServer: {
+//     historyApiFallback: true,
+//     open: true,
+//   },
+// };
+
+// old
+
 module.exports = {
   module: {
     rules: [
@@ -12,5 +88,9 @@ module.exports = {
         use: ["file-loader"],
       },
     ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
   },
 };
